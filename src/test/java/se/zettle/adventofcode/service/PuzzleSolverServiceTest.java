f79504c23cacdadb3e10e984a6aed7d7ffe61854
Anand Joshi <ananjoshi@paypal.com>
2021-12-02 08:19:50 +0100

    Test for PuzzleSolverService

--- src/test/java/se/zettle/adventofcode/service/PuzzleSolverServiceTest.java
+++ src/test/java/se/zettle/adventofcode/service/PuzzleSolverServiceTest.java
@@ -1,10 +1,23 @@
 package se.zettle.adventofcode.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PuzzleSolverServiceTest {
 
+    @Autowired
+    PuzzleSolverService puzzleSolverService;
+
+    @Test
+    void serviceRunsCorrectly() {
+
+        puzzleSolverService.run();
+
+        assertThat(puzzleSolverService).isNotNull();
+    }
 }
